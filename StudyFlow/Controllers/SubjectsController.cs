58ceb9b1e8b1ccad4a0a18190dfcf290267c2786
Andreabafa1444 <116229674+Andreabafa1444@users.noreply.github.com>
2025-12-18 23:37:50 -0600

    Feature day 2 evaluations (#2)
    * Evaluation Controller and Evaluation views
    
    * average and other changes on SubjectController

--- StudyFlow/Controllers/SubjectsController.cs
+++ StudyFlow/Controllers/SubjectsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using StudyFlow.Models;
 using StudyFlow.Data;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 namespace StudyFlow.Controllers
 {
   public class SubjectsController : Controller
@@ -13,11 +14,7 @@ namespace StudyFlow.Controllers
             _context = context;
         }
         //gest all subjects from the database and sends them to the view
-        public IActionResult Index()
-        {
-            var subjects = _context.Subjects.ToList();
-            return View(subjects);
-        }
+        
         [HttpGet]
         public IActionResult Create()
         {
@@ -33,6 +30,52 @@ namespace StudyFlow.Controllers
             _context.Subjects.Add(newsubject);
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
+        }
+        //now we are going to create the Details controller
+        [HttpGet]
+          public IActionResult Details(string id )
+        {
+            if(string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+            var subject = _context.Subjects
+                .Include(s=>s.Evaluations)
+                .FirstOrDefault(s=>s.Id == id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            double average = 0;
+            if(subject.Evaluations.Any())
+            {
+                average = subject.Evaluations
+                    .Sum(e => e.Grade * e.Percentage / 100.0);
+            }    
+            if (average >= 7.0)
+            {
+                subject.Status = SubjectStatus.Approved;
+            } 
+            else if(average >= 6.5)
+            {
+                subject.Status = SubjectStatus.AtRisk;
+            }       
+            else
+            {
+                subject.Status = SubjectStatus.Failed;
+            }
+            _context.SaveChanges();
+            ViewBag.Average = average;
+            return View(subject);
+        }
+        //mostramos todo 
+        public IActionResult Index()
+        {
+            var subjects = _context.Subjects
+                .Include (s => s.Evaluations)
+                .ToList();
+            return View(subjects);
         }
     } 
 }
